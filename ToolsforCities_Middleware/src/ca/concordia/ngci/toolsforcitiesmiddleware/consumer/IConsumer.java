4437e50cfe19028403dcb9b5de7b74fff650d2ea
Sikandar Ejaz <34721766+SikandarEjaz@users.noreply.github.com>
2024-04-29 15:55:11 -0400

    Delete ToolsforCities_Middleware directory

--- ToolsforCities_Middleware/src/ca/concordia/ngci/toolsforcitiesmiddleware/consumer/IConsumer.java
+++
@@ -1,7 +0,0 @@
-package ca.concordia.ngci.toolsforcitiesmiddleware.consumer;
-
-import java.util.List;
-
-public interface IConsumer<E> {
-	void receiveData(List<E> data);
-}
--- ToolsforCities_Middleware/src/ca/concordia/ngci/toolsforcitiesmiddleware/producer/IProducer.java
+++
@@ -1,7 +0,0 @@
-package ca.concordia.ngci.toolsforcitiesmiddleware.producer;
-
-import java.io.IOException;
-
-public interface IProducer<E> {
-	E fetchData() throws IOException;
-}
