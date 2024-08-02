0eb638e3dcf39c7ac1aec36d2610ba786927e62d
Gabriel Ullmann <gabriel.cavalheiroullmann@concordia.ca>
2024-08-02 17:02:24 -0400

    add examples with threads

--- Middleware/src/test/java/ca/concordia/ngci/tools4cities/middleware/test/Test1.java
+++ Middleware/src/test/java/ca/concordia/ngci/tools4cities/middleware/test/Test1.java
@@ -17,10 +17,9 @@ import ca.concordia.ngci.tools4cities.middleware.middleware.IProducer;
 
 public class Test1 {
 
-
 	@Test
 	public void test1() {
-		final IProducer<String> producer = new CSVProducer("./src/test/data/examplecsv.csv");
+		final IProducer<String> producer = new CSVProducer("./src/test/data/example.csv");
 		final Set<IProducer<String>> producers = new HashSet<IProducer<String>>();
 		producers.add(producer);
 		final IConsumer<String> consumer = new CSVConsumer(producers);
@@ -28,5 +27,41 @@ public class Test1 {
 		Assertions.assertEquals(loadedCSV.size(), 7);
 	}
 
+	@Test
+	public void test2() {
+		
+		final Object[] results = new Object[2];
+
+		Thread thread1 = new Thread(() -> {
+			final IProducer<String> producer1 = new CSVProducer("./src/test/data/example.csv");
+			final Set<IProducer<String>> producers1 = new HashSet<IProducer<String>>();
+			producers1.add(producer1);
+			final IConsumer<String> consumer1 = new CSVConsumer(producers1);
+			results[0] = consumer1;
+		});
+
+		Thread thread2 = new Thread(() -> {
+			final IProducer<String> producer2 = new CSVProducer("./src/test/data/larger.csv");
+			final Set<IProducer<String>> producers2 = new HashSet<IProducer<String>>();
+			producers2.add(producer2);
+			final IConsumer<String> consumer2 = new CSVConsumer(producers2);
+			results[1] = consumer2;
+		});
+		
+		// Start the threads
+        thread1.start();
+        thread2.start();
+
+        // Wait for the threads to finish
+        try {
+            thread1.join();
+            thread2.join();
+            Assertions.assertEquals(7, ((CSVConsumer) results[0]).getResults().size());
+            Assertions.assertEquals(0, ((CSVConsumer) results[1]).getResults().size());
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+	}
 
 }
