8af46e0b6715bd515ee88b052b380c14d952dbf6
Gabriel Ullmann <gabriel.cavalheiroullmann@concordia.ca>
2024-08-06 15:40:40 -0400

    uncomment tests

--- Middleware/src/test/java/ca/concordia/ngci/tools4cities/middleware/test/Test1.java
+++ Middleware/src/test/java/ca/concordia/ngci/tools4cities/middleware/test/Test1.java
@@ -35,68 +35,67 @@ public class Test1 {
 		Assertions.assertEquals(7, filteredCSV.size());
 	}
 
-//	@Test
-//	public void testCSVThreads() {
-//		final String postalCodePrefix = "H1B";
-//		final Object[] results = new Object[2];
-//
-//		// same as testCSVFilesystem, but with two concurrent calls dealing with different CSV files 
-//		Thread thread1 = new Thread(() -> {
-//			final IProducer<String> producer1 = new CSVProducer("./src/test/data/example.csv", null);
-//			final Set<IProducer<String>> producers1 = new HashSet<IProducer<String>>();
-//			producers1.add(producer1);
-//			final IConsumer<String> consumer1 = new CSVFilterConsumer(producers1, postalCodePrefix);
-//			results[0] = consumer1;
-//		});
-//
-//		Thread thread2 = new Thread(() -> {
-//			final IProducer<String> producer2 = new CSVProducer("./src/test/data/larger.csv", null);
-//			final Set<IProducer<String>> producers2 = new HashSet<IProducer<String>>();
-//			producers2.add(producer2);
-//			final IConsumer<String> consumer2 = new CSVFilterConsumer(producers2, postalCodePrefix);
-//			results[1] = consumer2;
-//		});
-//
-//		thread1.start();
-//		thread2.start();
-//
-//		try {
-//			thread1.join();
-//			thread2.join();
-//			Assertions.assertEquals(7, ((CSVFilterConsumer) results[0]).getResults().size());
-//			Assertions.assertEquals(0, ((CSVFilterConsumer) results[1]).getResults().size());
-//		} catch (InterruptedException e) {
-//			e.printStackTrace();
-//		}
-//
-//	}
-//
-//	@Test
-//	public void testJSON() {
-//		final RequestOptions requestOptions = new RequestOptions();
-//		requestOptions.method = "GET";
-//		requestOptions.contentType = "application/json";
-//
-//		String limitRecordCount = "100";
-//		String url = "https://www.donneesquebec.ca/recherche/api/3/action/datastore_search";
-//		String queryString = "?resource_id=05deae93-d9fc-4acb-9779-e0942b5e962f&limit=" + limitRecordCount;
-//
-//		// produce road collision data from Donées Québec
-//		final IProducer<JsonObject> producer = new JSONProducer(url + queryString, requestOptions);
-//		final Set<IProducer<JsonObject>> producers = new HashSet<IProducer<JsonObject>>();
-//		producers.add(producer);
-//
-//		// consume road collision data
-//		final IConsumer<JsonObject> consumer = new JSONConsumer(producers);
-//		List<JsonObject> roadCollisionJSON = consumer.getResults();
-//
-//		// access the "records" array inside the resulting object
-//		JsonObject result = roadCollisionJSON.get(0).get("result").getAsJsonObject();
-//		JsonArray records = result.get("records").getAsJsonArray();
-//
-//		// given limitRecordCount=100, there should be 100 records
-//		System.out.println(records.get(1).getAsJsonObject().get("NB_VEH_IMPLIQUES_ACCDN"));
-//		Assertions.assertEquals(100, records.size());
-//	}
+	@Test
+	public void testCSVThreads() {
+		final Object[] results = new Object[2];
+
+		// same as testCSVFilesystem, but with two concurrent calls dealing with different CSV files 
+		Thread thread1 = new Thread(() -> {
+			final IProducer<String> producer1 = new CSVProducer("./src/test/data/example.csv", null);
+			final Set<IProducer<String>> producers1 = new HashSet<IProducer<String>>();
+			producers1.add(producer1);
+			final IConsumer<String> consumer1 = new CSVFilterConsumer(producers1);
+			results[0] = consumer1;
+		});
+
+		Thread thread2 = new Thread(() -> {
+			final IProducer<String> producer2 = new CSVProducer("./src/test/data/larger.csv", null);
+			final Set<IProducer<String>> producers2 = new HashSet<IProducer<String>>();
+			producers2.add(producer2);
+			final IConsumer<String> consumer2 = new CSVFilterConsumer(producers2);
+			results[1] = consumer2;
+		});
+
+		thread1.start();
+		thread2.start();
+
+		try {
+			thread1.join();
+			thread2.join();
+			Assertions.assertEquals(7, ((CSVFilterConsumer) results[0]).getResults().size());
+			Assertions.assertEquals(0, ((CSVFilterConsumer) results[1]).getResults().size());
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+
+	}
+
+	@Test
+	public void testJSON() {
+		final RequestOptions requestOptions = new RequestOptions();
+		requestOptions.method = "GET";
+		requestOptions.contentType = "application/json";
+
+		String limitRecordCount = "100";
+		String url = "https://www.donneesquebec.ca/recherche/api/3/action/datastore_search";
+		String queryString = "?resource_id=05deae93-d9fc-4acb-9779-e0942b5e962f&limit=" + limitRecordCount;
+
+		// produce road collision data from Donées Québec
+		final IProducer<JsonObject> producer = new JSONProducer(url + queryString, requestOptions);
+		final Set<IProducer<JsonObject>> producers = new HashSet<IProducer<JsonObject>>();
+		producers.add(producer);
+
+		// consume road collision data
+		final IConsumer<JsonObject> consumer = new JSONConsumer(producers);
+		List<JsonObject> roadCollisionJSON = consumer.getResults();
+
+		// access the "records" array inside the resulting object
+		JsonObject result = roadCollisionJSON.get(0).get("result").getAsJsonObject();
+		JsonArray records = result.get("records").getAsJsonArray();
+
+		// given limitRecordCount=100, there should be 100 records
+		System.out.println(records.get(1).getAsJsonObject().get("NB_VEH_IMPLIQUES_ACCDN"));
+		Assertions.assertEquals(100, records.size());
+	}
 
 }
