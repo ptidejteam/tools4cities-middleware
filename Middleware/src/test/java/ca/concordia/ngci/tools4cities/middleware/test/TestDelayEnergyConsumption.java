eb070e218dd457471be21613cec3a04b15767235
Meyo-Ndo Z. <minettemeyo@gmail.com>
2024-10-22 14:46:44 -0400

    Update TestDelayEnergyConsumption.java

--- Middleware/src/test/java/ca/concordia/ngci/tools4cities/middleware/test/TestDelayEnergyConsumption.java
+++ Middleware/src/test/java/ca/concordia/ngci/tools4cities/middleware/test/TestDelayEnergyConsumption.java
@@ -19,11 +19,9 @@ public class TestDelayEnergyConsumption {
         producers.add(producer1);
         producers.add(producer2);
         producers.add(producer3);
-
-     // Create the consumer
+        
         final DelayEnergyConsumptionConsumer consumer = new DelayEnergyConsumptionConsumer(producers);
 
-     // Trigger data fetch
         try {
             producer1.fetchData();
             producer2.fetchData();
@@ -32,15 +30,12 @@ public class TestDelayEnergyConsumption {
             e.printStackTrace();
         }
 
-     // Add necessary waiting time to allow the processing to end before time is collected
         Thread.sleep(1000); 
 
-        // Check the results
         List<String> results = consumer.getResults();
         
         System.out.println("Final results collected: " + results);
         
-     // Ensure fetchData() was only called once for each producer
         long totalFetchingTime = ((DelayEnergyConsumptionProducer) producer1).getFetchTime() +
                                  ((DelayEnergyConsumptionProducer) producer2).getFetchTime() +
                                  ((DelayEnergyConsumptionProducer) producer3).getFetchTime();
@@ -56,15 +51,12 @@ public class TestDelayEnergyConsumption {
         // Display the total processing time, check if it is the sum of individual fetching time and processing time
         System.out.println("Total time calculated for all operations: " + totalProcessingTime + " ms");
 
-        // Validate the sum of the values (update expectedSum based on your actual data)
         Double expectedSum = 19.0; // Always adjust based on the actual CSV data
         System.out.println("Calculated Sum at consumer's: " + consumer.getSum());
         
         Assertions.assertEquals(expectedSum, consumer.getSum(), "The sum of the values should be correct.");
-
-        // Validate the results list
-        List<String> expectedResults = Arrays.asList("3", "6", "10"); // Always adjust based on your expected results
+        
+        List<String> expectedResults = Arrays.asList("3", "6", "10"); 
         Assertions.assertEquals(expectedResults, results, "The results list should match the expected values.");
-        Assertions.assertTrue(totalProcessingTime > 0, "Total processing time should be greater than zero.");
     }  
 }
