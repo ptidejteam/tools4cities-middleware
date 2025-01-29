fd8ef85eebbd99fdcbdfc6c85b4274c2e216d25a
SikandarEjaz <sikanderejaz1@gmail.com>
2025-01-29 14:21:43 -0500

    remove unused folders

--- Middleware/src/main/java/ca/concordia/ngci/tools4cities/middleware/middleware/IOperation.java
+++
@@ -1,9 +0,0 @@
-package ca.concordia.ngci.tools4cities.middleware.middleware;
-
-import java.util.List;
-
-public interface IOperation {
-
-	<E> List<E> perform(List<E> inputs) throws Exception;
-
-}
