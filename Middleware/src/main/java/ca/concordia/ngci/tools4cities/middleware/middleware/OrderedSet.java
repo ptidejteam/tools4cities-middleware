5c7b39016411dcfa359f07a821bb21641744ada9
SikandarEjaz <sikanderejaz1@gmail.com>
2025-01-29 14:04:21 -0500

    delete unused files

--- Middleware/src/main/java/ca/concordia/ngci/tools4cities/middleware/middleware/OrderedSet.java
+++
@@ -1,8 +0,0 @@
-package ca.concordia.ngci.tools4cities.middleware.middleware;
-
-import java.util.List;
-
-public interface OrderedSet<E> {
-	void IOperations(final List<E> operations);
-
-}
--- Spirngboot Middleware/src/main/java/com/concordia/middleware/core/OrderedSet.java
+++
@@ -1,8 +0,0 @@
-package com.concordia.middleware.core;
-
-import java.util.List;
-
-public interface OrderedSet<E> {
-	void IOperations(final List<E> operations);
-
-}
