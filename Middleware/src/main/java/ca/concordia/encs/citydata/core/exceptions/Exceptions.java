3181e0924e9415e46eda7fc253ed2e1bf3434e95
SikandarEjaz <sikanderejaz1@gmail.com>
2025-02-06 22:17:57 -0500

    added some tests for /apply/sync

---
+++ Middleware/src/main/java/ca/concordia/encs/citydata/core/exceptions/Exceptions.java
@@ -0,0 +1,10 @@
+package ca.concordia.encs.citydata.core.exceptions;
+
+/* This java class contains definitions of custom exceptions
+ * Author: Sikandar Ejaz
+ * Date: 2-6-2025
+ */
+
+public class Exceptions {
+
+}
