bcc338d04b3c669cef95523ddbfa44525c487532
Rushin-Makwana <rushymakwana@gmail.com>
2025-02-11 23:26:15 -0500

    conflicts resolved

--- Middleware/src/test/java/ca/concordia/encs/citydata/ApplyTest.java
+++ Middleware/src/test/java/ca/concordia/encs/citydata/ApplyTest.java
@@ -1,8 +1,7 @@
 package ca.concordia.encs.citydata;
 
-import static org.hamcrest.CoreMatchers.containsString;
-import static org.hamcrest.Matchers.anyOf;
 import static org.hamcrest.Matchers.containsString;
+import static org.hamcrest.Matchers.anyOf;
 import static org.junit.jupiter.api.Assertions.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
@@ -11,7 +10,6 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 
 import ca.concordia.encs.citydata.core.ReflectionUtils;
-import org.hamcrest.CoreMatchers;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
@@ -30,9 +28,7 @@ import java.lang.reflect.Method;
 
 @SpringBootTest(classes = AppConfig.class)
 @AutoConfigureMockMvc
-@ComponentScan(basePackages = "ca.concordia.encs.citydata.core") // Explicitly scan the package containing
-// ProducerController (now ApplyController)
-
+@ComponentScan(basePackages = "ca.concordia.encs.citydata.core")
 public class ApplyTest {
 
 	@Autowired
@@ -103,7 +99,7 @@ public class ApplyTest {
 	private void performPostRequest(String url, String contentType, String content) throws Exception {
 		mockMvc.perform(post(url).contentType(contentType).content(content))
 				.andExpect(status().isOk())
-				.andExpect(content().string(CoreMatchers.containsString("result")));
+				.andExpect(content().string(containsString("result")));
 	}
 
 	// Test for valid steps
@@ -114,17 +110,16 @@ public class ApplyTest {
 				.andExpect(status().isOk()).andExpect(content().string(containsString("Hello! The runner")));
 	}
 
-	// Test to check /apply/async with invalid JSON input
+	// Test to check /apply/async with invalid JSON input -- Need to fix
 	@Test
 	public void whenInvalidReturnIdWrongInput() throws Exception {
 		final String invalidSteps = "invalid-json";
 
 		mockMvc.perform(post("/apply/async").contentType(MediaType.APPLICATION_JSON).content(invalidSteps))
-				.andExpect(status().isOk()).andExpect(content().string(containsString(
-						"{\"runnerError\":\"java.lang.IllegalStateException: Not a JSON Object: \\\"invalid-json\\\"\"}")));
+				.andExpect(status().isOk()).andExpect(content().string(containsString("{\"runnerError\":\"java.lang.IllegalStateException: Not a JSON Object: \\\"invalid-json\\\"\"}")));
 	}
 
-	// Test to check /apply/async with invalid and unexpected JSON input type
+	// Test to check /apply/async with invalid and unexpected JSON input type -- Need to fix
 	@Test
 	public void whenInvalidReturnIdWrongMediaType() throws Exception {
 		final String invalidSteps = "invalid-json";
@@ -133,23 +128,6 @@ public class ApplyTest {
 						"{\"runnerError\":\"java.lang.IllegalStateException: Not a JSON Object: \\\"invalid-json\\\"\"}")));
 	}
 
-	// Test to check /apply/async with invalid JSON input and wrong route accessed
-	@Test
-	public void whenInvalidReturnIdWrongRouteAccess() throws Exception {
-		final String invalidSteps = "invalid-json";
-
-		mockMvc.perform(post("/apply/asyncXXX").contentType(MediaType.APPLICATION_JSON).content(invalidSteps))
-				.andExpect(status().is5xxServerError());
-	}
-
-	// Test to check /apply/async with invalid JSON input and wrong route accessed
-	@Test
-	public void whenInvalidReturnId4() throws Exception {
-		final String invalidSteps = "invalid-json";
-
-		mockMvc.perform(post("/applyXXX/async").contentType(MediaType.APPLICATION_NDJSON).content(invalidSteps))
-				.andExpect(status().is5xxServerError());
-	}
 
 	// Test for GET /async/{runnerId} with a valid runner ID
 
@@ -162,7 +140,7 @@ public class ApplyTest {
 						containsString("\"result\":"))));
 	}
 
-	// Test for invalid runner ID
+	// Test for invalid runner ID   Need to fix
 	@Test
 	public void whenInvalidRunnerId_thenReturnNotReadyMessage() throws Exception {
 		String invalidRunnerId = "nonexistent-runner-id";
@@ -193,7 +171,7 @@ public class ApplyTest {
 	public void testSyncWrongMediaTypeAccess() throws Exception {
 		String jsonPayload = createJsonPayload();
 		mockMvc.perform(post("/apply/sync").contentType("XXX").content(jsonPayload))
-				.andExpect(status().is4xxClientError());
+				.andExpect(status().is5xxServerError());
 	}
 
 	// Test for sync with wrong media type
@@ -304,82 +282,6 @@ public class ApplyTest {
 		Method method = ReflectionUtils.findSetterMethod(StringBuilder.class, "length", new JsonObject());
 		assertNotNull(method);
 		assertEquals("setLength", method.getName());
-	}
-
-
-	@Test
-	public void testCapitalize() {
-		assertEquals("Test", ReflectionUtils.capitalize("test"));
-		assertEquals("Test", ReflectionUtils.capitalize("Test"));
-		assertEquals("", ReflectionUtils.capitalize(""));
-		assertNull(ReflectionUtils.capitalize(null));
-	}
-
-	// Test to check /apply/sync with valid input
-	@Test
-	public void testSync() throws Exception {
-		JsonObject payload = new JsonObject();
-		payload.addProperty("use", "ca.concordia.encs.citydata.producers.StringProducer");
-		// Add "withParams" array
-		JsonArray withParams = new JsonArray();
-		JsonObject param1 = new JsonObject();
-		param1.addProperty("name", "generationProcess");
-		param1.addProperty("value", "random");
-		withParams.add(param1);
-		JsonObject param2 = new JsonObject();
-		param2.addProperty("name", "stringLength");
-		param2.addProperty("value", 10);
-		withParams.add(param2);
-		payload.add("withParams", withParams);
-		// Add "apply" array
-		JsonArray apply = new JsonArray();
-		// First operation
-		JsonObject operation1 = new JsonObject();
-		operation1.addProperty("name", "ca.concordia.encs.citydata.operations.StringReplaceOperation");
-		JsonArray operation1Params = new JsonArray();
-		JsonObject operation1Param1 = new JsonObject();
-		operation1Param1.addProperty("name", "searchFor");
-		operation1Param1.addProperty("value", "a");
-		operation1Params.add(operation1Param1);
-		JsonObject operation1Param2 = new JsonObject();
-		operation1Param2.addProperty("name", "replaceBy");
-		operation1Param2.addProperty("value", "b");
-		operation1Params.add(operation1Param2);
-		operation1.add("withParams", operation1Params);
-		apply.add(operation1);
-		// Second operation
-		JsonObject operation2 = new JsonObject();
-		operation2.addProperty("name", "ca.concordia.encs.citydata.operations.StringReplaceOperation");
-		JsonArray operation2Params = new JsonArray();
-		JsonObject operation2Param1 = new JsonObject();
-		operation2Param1.addProperty("name", "searchFor");
-		operation2Param1.addProperty("value", "cxsffdf");
-		operation2Params.add(operation2Param1);
-		JsonObject operation2Param2 = new JsonObject();
-		operation2Param2.addProperty("name", "replaceBy");
-		operation2Param2.addProperty("value", "c");
-		operation2Params.add(operation2Param2);
-		operation2.add("withParams", operation2Params);
-		apply.add(operation2);
-		payload.add("apply", apply);
-		// Convert the payload to a JSON string
-		ApplyTest.jsonPayload = payload.toString();
-		mockMvc.perform(post("/apply/sync").contentType("application/json").content(ApplyTest.jsonPayload))
-				.andExpect(status().isOk()).andExpect(content().string(containsString("result")));
-	}
-
-	// Test to check /apply/sync with invalid input
-	@Test
-	public void testSyncWrongMediaTypeAccess() throws Exception {
-		mockMvc.perform(post("/apply/sync").contentType("XXX").content(ApplyTest.jsonPayload))
-				.andExpect(status().is5xxServerError());
-	}
-
-	// Test to check /apply/sync with incomplete input - Fix this error
-	@Test
-	public void testSyncWrongMediaType() throws Exception {
-		mockMvc.perform(post("/apply/sync").contentType("application/XXX").content(ApplyTest.jsonPayload))
-				.andExpect(status().is2xxSuccessful());
 	}
 
 }
