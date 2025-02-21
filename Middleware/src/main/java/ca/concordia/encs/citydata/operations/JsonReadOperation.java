15caf7216b7286c64d973bbf83b289cf7cd9dcdc
Gabriel Ullmann <gabriel.cavalheiroullmann@concordia.ca>
2025-02-21 16:39:23 -0500

    add json read operation

--- Middleware/src/main/java/ca/concordia/encs/citydata/operations/JsonReadOperation.java
+++ Middleware/src/main/java/ca/concordia/encs/citydata/operations/JsonReadOperation.java
@@ -20,56 +20,91 @@ public class JsonReadOperation extends AbstractOperation<JsonObject> implements 
 		this.path = path;
 	}
 
+	private void handleJsonElementType(JsonElement element, String key) {
+		if (element.isJsonArray()) {
+			this.currentObject = element.getAsJsonArray();
+		} else if (element.isJsonPrimitive() || this.currentObject.isJsonNull()) {
+			final JsonObject objectWrapper = new JsonObject();
+			objectWrapper.addProperty(key, element.getAsString());
+			this.currentObject = objectWrapper;
+		} else {
+			this.currentObject = element.getAsJsonObject();
+		}
+	}
+
+	private void handlePathNotFound(String key) {
+		final JsonObject errorWrapper = new JsonObject();
+
+		if (this.currentObject == null || this.currentObject.isJsonNull()) {
+			errorWrapper.addProperty("error", "The path \"" + this.path + "\" cannot be found.");
+		} else if (this.currentObject.isJsonObject()) {
+			errorWrapper.addProperty("error",
+					"The key \"" + key + "\" cannot be found. You can try one of the following keys instead: "
+							+ this.currentObject.getAsJsonObject().keySet().toString());
+		} else {
+			String lastKnowGoodObject = this.currentObject.toString();
+			String excerpt = lastKnowGoodObject.length() > 50 ? lastKnowGoodObject.substring(0, 50) + "..."
+					: lastKnowGoodObject;
+			errorWrapper.addProperty("error", "The key \"" + key + "\" cannot be found in: " + excerpt);
+		}
+
+		this.currentObject = errorWrapper;
+	}
+
 	@Override
 	public ArrayList<JsonObject> apply(ArrayList<JsonObject> inputs) {
 
 		final ArrayList<JsonObject> matchingJsonObjects = new ArrayList<>();
 		final String[] pathParts = this.path.split("\\.");
 
-		for (JsonObject input : inputs) {
+		if (pathParts.length > 0) {
 
-			this.currentObject = input;
-			for (String key : pathParts) {
+			for (JsonObject input : inputs) {
 
-				// check if key contains []
-				final Matcher arrayAccessMatcher = this.containsArrayAccess.matcher(key);
+				this.currentObject = input;
+				for (String key : pathParts) {
 
-				// if so, go to key and array position
-				if (arrayAccessMatcher.matches()) {
-					final String arrayKey = arrayAccessMatcher.group(1);
-					final String arrayPositionString = arrayAccessMatcher.group(2);
+					try {
+						// check if key contains []
+						final Matcher arrayAccessMatcher = this.containsArrayAccess.matcher(key);
 
-					// before reading array position, make sure you are reading from a JsonArray
-					if (this.currentObject.isJsonObject()) {
-						final JsonArray jsonArray = ((JsonObject) this.currentObject).get(arrayKey).getAsJsonArray();
-						final int arrayPosition = Integer.parseInt(arrayPositionString);
-						final JsonElement currentElement = jsonArray.get(arrayPosition);
-						if (currentElement.isJsonArray()) {
-							this.currentObject = currentElement.getAsJsonArray();
+						// if so, go to key and array position
+						if (this.currentObject.isJsonObject() && arrayAccessMatcher.matches()) {
+							final String arrayKey = arrayAccessMatcher.group(1);
+							final String arrayPositionString = arrayAccessMatcher.group(2);
+							final JsonArray jsonArray = ((JsonObject) this.currentObject).get(arrayKey)
+									.getAsJsonArray();
+							final int arrayPosition = Integer.parseInt(arrayPositionString);
+							final JsonElement currentElement = jsonArray.get(arrayPosition);
+							this.handleJsonElementType(currentElement, key);
+
+							// else, it is not an array, so attempt to read it as an object
+						} else if (this.currentObject.isJsonObject()) {
+							// this object may contain another object, an array or primitive (e.g. a number)
+							final JsonElement currentElement = ((JsonObject) this.currentObject).get(key);
+							this.handleJsonElementType(currentElement, key);
 						} else {
-							this.currentObject = currentElement.getAsJsonObject();
+							// this means the current object is either a primitive or null
+							break;
 						}
-					} else {
-						// if not, halt immediately
-						matchingJsonObjects.add(this.currentObject.getAsJsonObject());
-						return matchingJsonObjects;
-					}
-
-				} else {
-
-					if (this.currentObject.isJsonObject()) {
-						final JsonElement currentElement = ((JsonObject) this.currentObject).get(key);
-						if (currentElement.isJsonObject()) {
-							this.currentObject = currentElement.getAsJsonObject();
-						} else {
-							this.currentObject = currentElement.getAsJsonArray();
-						}
+					} catch (Exception e) {
+						this.handlePathNotFound(key);
+						break;
 					}
 
 				}
 
+				// if the final result is object, return object, else add return to a wrapper
+				if (this.currentObject.isJsonObject()) {
+					matchingJsonObjects.add(this.currentObject.getAsJsonObject());
+				} else {
+					final JsonObject objectWrapper = new JsonObject();
+					objectWrapper.addProperty(pathParts[pathParts.length - 1], this.currentObject.toString());
+					matchingJsonObjects.add(objectWrapper);
+				}
+
 			}
-			matchingJsonObjects.add(this.currentObject.getAsJsonObject());
+
 		}
 
 		return matchingJsonObjects;
