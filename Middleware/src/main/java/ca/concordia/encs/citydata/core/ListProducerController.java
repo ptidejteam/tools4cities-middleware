cccb9dd8d30eba911dd21362d9e29dcb61e4f5cf
Gabriel Ullmann <gabriel.cavalheiroullmann@concordia.ca>
2025-03-12 14:37:35 -0400

    chchchanges

--- Middleware/src/main/java/ca/concordia/encs/citydata/core/ListProducerController.java
+++ Middleware/src/main/java/ca/concordia/encs/citydata/core/ListProducerController.java
@@ -22,52 +22,62 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/producers")
 public class ListProducerController {
 
-
 	@GetMapping("/list")
 	public List<Map<String, String>> listProducers() throws IOException, ClassNotFoundException {
-		List<Map<String, String>> producersDetailsList = new ArrayList<>();
+		System.out.println("Starting!!!");
+		try {
 
-		// Get the path to the package
-		URL packageURL = Thread.currentThread().getContextClassLoader().getResource(Constants.PRODUCER_ROOT_PACKAGE);
+			List<Map<String, String>> producersDetailsList = new ArrayList<>();
 
-		if (packageURL == null) {
-			return List.of(Map.of("error", "Package not found."));
-		}
+			// Get the path to the package
+			URL packageURL = Thread.currentThread().getContextClassLoader()
+					.getResource(Constants.PRODUCER_ROOT_PACKAGE);
 
-		// Scan for class files in the package directory
-		File[] files = new File(packageURL.getFile()).listFiles((dir, name) -> name.endsWith(".class"));
-
-		if (files != null) {
-			for (File file : files) {
-				// Remove .class extension
-				String className = file.getName().substring(0, file.getName().length() - 6);
-
-				// Load the class using reflection
-				Class<?> clazz = Class.forName("ca.concordia.encs.citydata.producers." + className);
-
-				// Map to hold operation details
-				Map<String, String> producersDetails = new HashMap<>();
-
-				// Set class name
-				producersDetails.put("name", clazz.getName());
-
-				// Collect fields and method signatures
-				List<String> paramList = new ArrayList<>();
-
-				// List fields
-				Field[] fields = clazz.getDeclaredFields();
-				for (Field field : fields) {
-					paramList.add(field.getName() + " (" + field.getType().getSimpleName() + ")");
-				}
-
-				producersDetails.put("params", String.join(", ", paramList));
-
-				producersDetailsList.add(producersDetails);
+			if (packageURL == null) {
+				return List.of(Map.of("error", "Package not found."));
 			}
-		}
 
-		return producersDetailsList.isEmpty()
-				? List.of(Map.of("message", "No producers found in package: " + Constants.PRODUCER_ROOT_PACKAGE))
-				: producersDetailsList;
+			// Scan for class files in the package directory
+			File[] files = new File(packageURL.getFile()).listFiles((dir, name) -> name.endsWith(".class"));
+
+			if (files != null) {
+				for (File file : files) {
+					// Remove .class extension
+					String className = file.getName().substring(0, file.getName().length() - 6);
+
+					// Load the class using reflection
+					Class<?> clazz = Class.forName("ca.concordia.encs.citydata.producers." + className);
+
+					// Map to hold operation details
+					Map<String, String> producersDetails = new HashMap<>();
+
+					// Set class name
+					producersDetails.put("name", clazz.getName());
+
+					// Collect fields and method signatures
+					List<String> paramList = new ArrayList<>();
+
+					// List fields
+					Field[] fields = clazz.getDeclaredFields();
+					for (Field field : fields) {
+						paramList.add(field.getName() + " (" + field.getType().getSimpleName() + ")");
+					}
+
+					producersDetails.put("params", String.join(", ", paramList));
+
+					producersDetailsList.add(producersDetails);
+				}
+			}
+
+			return producersDetailsList.isEmpty()
+					? List.of(Map.of("message", "No producers found in package: " + Constants.PRODUCER_ROOT_PACKAGE))
+					: producersDetailsList;
+		} catch (Exception ex) {
+			System.out.println("Error caught!!!");
+			ex.printStackTrace();
+		}
+		System.out.println("Ending!!!");
+		return null;
 	}
+
 }
