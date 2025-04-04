8d313dcea2ed0d63e10543e2d3a28bca7143179a
Gabriel Ullmann <gabriel.cavalheiroullmann@concordia.ca>
2025-04-04 11:55:26 -0400

    test

---
+++ Middleware/src/test/java/ca/concordia/encs/citydata/VarsTest.java
@@ -0,0 +1,30 @@
+package ca.concordia.encs.citydata;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.junit.Assert.assertEquals;
+
+import org.hamcrest.CoreMatchers;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.ComponentScan;
+
+import ca.concordia.encs.citydata.core.AppConfig;
+
+@SpringBootTest(classes = AppConfig.class)
+@AutoConfigureMockMvc
+@ComponentScan(basePackages = "ca.concordia.encs.citydata.core")
+public class VarsTest {
+
+	// Test for valid steps
+	@Test
+	public void test1() throws Exception {
+		String sensitive = System.getenv("CITYDATA_BLA");
+		String nonSensitive = System.getenv("CITYDATA_NOT_SENSITIVE");
+
+		assertEquals(nonSensitive, "abc123");
+		assertThat(sensitive, CoreMatchers.containsString("p"));
+
+	}
+
+}
