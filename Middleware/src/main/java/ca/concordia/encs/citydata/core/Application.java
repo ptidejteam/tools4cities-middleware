27cee6270a1eb66b41b9b8c0f06e2dd7df5d20d0
Gabriel Ullmann <gabriel.cavalheiroullmann@concordia.ca>
2025-04-30 14:26:05 -0400

    remove misplaced code

--- Middleware/src/main/java/ca/concordia/encs/citydata/core/Application.java
+++ Middleware/src/main/java/ca/concordia/encs/citydata/core/Application.java
@@ -1,17 +1,5 @@
 package ca.concordia.encs.citydata.core;
 
-import java.io.IOException;
-import java.nio.ByteBuffer;
-import java.nio.ByteOrder;
-import java.time.LocalDateTime;
-import java.time.ZoneOffset;
-import java.time.format.DateTimeFormatter;
-
-import org.apache.avro.generic.GenericRecord;
-import org.apache.hadoop.conf.Configuration;
-import org.apache.hadoop.fs.Path;
-import org.apache.parquet.avro.AvroParquetReader;
-import org.apache.parquet.hadoop.ParquetReader;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
@@ -38,62 +26,10 @@ public class Application {
 	DiskDatastore diskStore = DiskDatastore.getInstance();
 	MongoDataStore mongoDataStore = MongoDataStore.getInstance();
 
-	public static String convertInt96ToDateTimeString(byte[] int96Bytes) {
-		if (int96Bytes.length != 12) {
-			throw new IllegalArgumentException("INT96 must be exactly 12 bytes");
-		}
-
-		// INT96 is little-endian
-		ByteBuffer buffer = ByteBuffer.wrap(int96Bytes).order(ByteOrder.LITTLE_ENDIAN);
-
-		long nanosOfDay = buffer.getLong(); // first 8 bytes: nanos since midnight
-		int julianDay = buffer.getInt(); // last 4 bytes: Julian day
-
-		// Convert Julian day to epoch day (days since 1970-01-01)
-		long epochDay = julianDay - 2440588L;
-		long secondsSinceEpoch = epochDay * 86400 + (nanosOfDay / 1_000_000_000);
-		int nanos = (int) (nanosOfDay % 1_000_000_000);
-
-		// Convert to LocalDateTime (UTC)
-		LocalDateTime dateTime = LocalDateTime.ofEpochSecond(secondsSinceEpoch, nanos, ZoneOffset.UTC);
-
-		// Format as string
-		return dateTime.format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss"));
-	}
-
 	public static void main(String[] args) throws IllegalArgumentException {
 		ApplicationContext context = SpringApplication.run(Application.class, args);
 		MongoDataStore mongoDataStore = context.getBean(MongoDataStore.class);
 
-		String filePath = "/Users/gabrielullmann/Desktop/OPE_Consommation.parquet";
-
-		// Create a configuration object
-		Configuration configuration = new Configuration();
-
-		// Enable the flag for reading INT96 as a byte array
-		configuration.setBoolean("parquet.avro.readInt96AsFixed", true);
-
-		// Build the AvroParquetReader with the configuration
-		try (ParquetReader<GenericRecord> reader = AvroParquetReader.<GenericRecord>builder(new Path(filePath))
-				.withConf(configuration) // Pass the configuration with INT96 flag
-				.build()) {
-
-			GenericRecord record;
-			int rowIndex = 0;
-			while ((record = reader.read()) != null && rowIndex < 100) {
-
-				if ("0.7686".equals(record.get("energieactivelivree_kwh").toString())) {
-					System.out.println("Row " + rowIndex + ": " + record);
-					System.out.println(convertInt96ToDateTimeString((byte[]) record.get("dateinterval")));
-				}
-
-				rowIndex++;
-			}
-
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
 	}
 
 }
