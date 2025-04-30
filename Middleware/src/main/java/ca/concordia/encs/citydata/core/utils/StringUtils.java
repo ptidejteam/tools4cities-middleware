0c765f2e01462c3e5531f1ef388876d2d6dcc298
Gabriel Ullmann <gabriel.cavalheiroullmann@concordia.ca>
2025-04-30 14:54:34 -0400

    add debug aid

--- Middleware/src/main/java/ca/concordia/encs/citydata/core/utils/StringUtils.java
+++ Middleware/src/main/java/ca/concordia/encs/citydata/core/utils/StringUtils.java
@@ -49,7 +49,8 @@ public abstract class StringUtils {
 				value = envVariables.getAsJsonObject().get(variableKey).getAsString();
 			}
 		}
-		System.out.println("===== " + variableKey + " corresponds to a " + value.length() + "-letter value.");
+		System.out.println("===== " + variableKey + " corresponds to a " + value.length()
+				+ "-letter value. The last letter is : " + value.substring(value.length() - 1));
 		return value;
 	}
 
